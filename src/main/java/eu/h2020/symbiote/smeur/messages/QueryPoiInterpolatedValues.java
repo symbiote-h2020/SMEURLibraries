f09942dbaae78073da5693d3901cc6cd0549d5db
Gerhard Duennebeil <Gerhard.Duennebeil@ait.ac.at>
2017-09-11 16:28:29 +0200

    One more change (A TODO mark I don't want to forget)

--- src/main/java/eu/h2020/symbiote/smeur/messages/QueryPoiInterpolatedValues.java
+++ src/main/java/eu/h2020/symbiote/smeur/messages/QueryPoiInterpolatedValues.java
@@ -6,12 +6,10 @@ import eu.h2020.symbiote.cloud.model.data.observation.Location;
 
 public class QueryPoiInterpolatedValues {
 	
-	public String regionID;	// Must refer to a previously registered region.
+	public String regionID;	// TODO: Remove this field when it's clear it's not in use anymore. 
 	
 	public Map<String, Location> thePoints;	// The strings are used as IDs for the points and will re-occur in the response.
 											// You can use human readable IDs to easy debugging,
 											// The interpolator will not interpret the IDs at all.
-
-	// Note: This will give you all interpolated properties.
 
 }
