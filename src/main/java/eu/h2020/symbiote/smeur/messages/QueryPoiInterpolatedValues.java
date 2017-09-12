013b7726d593e6a2eca5210ee8bd72574f9c1f48
Gerhard Duennebeil <Gerhard.Duennebeil@ait.ac.at>
2017-09-12 07:56:33 +0200

    Removed the now unencessary field "regionID" from PoIQuery.

--- src/main/java/eu/h2020/symbiote/smeur/messages/QueryPoiInterpolatedValues.java
+++ src/main/java/eu/h2020/symbiote/smeur/messages/QueryPoiInterpolatedValues.java
@@ -6,8 +6,6 @@ import eu.h2020.symbiote.cloud.model.data.observation.Location;
 
 public class QueryPoiInterpolatedValues {
 	
-	public String regionID;	// TODO: Remove this field when it's clear it's not in use anymore. 
-	
 	public Map<String, Location> thePoints;	// The strings are used as IDs for the points and will re-occur in the response.
 											// You can use human readable IDs to easy debugging,
 											// The interpolator will not interpret the IDs at all.
